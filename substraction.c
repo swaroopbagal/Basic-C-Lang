127b458e75e9e4dd38844525b7c15fb37fd67830
Swaroop Bagal <73351131+swaroopbagal@users.noreply.github.com>
2021-07-17 14:30:00 +0530

    Add files via upload

---
+++ substraction.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("\n enter the first number");
+    scanf("%d",&a);
+    printf("\n enter the second number");
+    scanf("%d",&b);
+    c=a-b;
+    printf("%d",c);
+    return 0;
+}
